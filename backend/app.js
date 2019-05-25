662c9a6454867138ab2598550996f5f996250f2b
Michail Chatzianastasis <mixalisx97@gmail.com>
2019-05-25 18:54:43 +0300

    Allow CORS: Access-Control-Allow-Origin

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const cors=require("./cors");
 
 //const productRoutes = require("./api/routes/products");
 //const orderRoutes = require("./api/routes/orders");
@@ -18,19 +19,8 @@ app.use(morgan("dev"));
 //app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors);
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
-  next();
-});
 
 // Routes which should handle requests
 
