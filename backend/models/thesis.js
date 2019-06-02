f4541e18ff5a086ab613cf194f7b3f9be2686af2
Michail Chatzianastasis <mixalisx97@gmail.com>
2019-06-03 01:22:02 +0300

    Fix thesis model

--- backend/models/thesis.js
+++ backend/models/thesis.js
@@ -11,9 +11,9 @@ const thesisSchema = mongoose.Schema({
     tags: [String],
     created_time: Date,
     assigned: Boolean, // true if it is assigned to some student
-    pending: Boolean, // external,student wait for approval from professor. 
     university: { type: mongoose.Schema.Types.ObjectId, ref: 'University'},
     professor: { type: mongoose.Schema.Types.ObjectId, ref:'User'},
+    pending: Boolean, // external,student wait for approval from professor. 
     creator_student: { type: mongoose.Schema.Types.ObjectId, ref:'User' },
     creator_external:{ type: mongoose.Schema.Types.ObjectId, ref:'External'},
     supervisor: [{ type : mongoose.Schema.Types.ObjectId, ref: 'User' }]
