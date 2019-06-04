59de9685f30ecfcab694817181f29b33c29b7328
Michail Chatzianastasis <mixalisx97@gmail.com>
2019-06-04 20:05:19 +0300

    Student routes: Can propose a thesis to professor

--- backend/routes/student.js
+++ backend/routes/student.js
@@ -13,9 +13,16 @@ router.delete("/request/:requestId",checkAuth,studentController.delete_request);
 
 router.get("/request/accepted",checkAuth,studentController.get_accepted_request); // get all accepted requests from professors
 router.get("/request/:requestId/accepted",checkAuth,studentController.get_accepted_request_byId); // get accepted request by Id
-router.post("/request/:requestId/accepted",checkAuth,studentController.post_accepted_request,studentController.delete_all_requests);// confirm request, establish connection(Student,Professor,Thesis) , remove request
+router.post("/request/:requestId/accepted",checkAuth,studentController.post_accepted_request,studentController.delete_all_requests);// confirm request, establish connection(Student,Professor,Thesis) , remove requests of student
 
 
+router.get("/pending/accepted",checkAuth,studentController.get_accepted) // get all accepted pending thesis of userId (From different professors) returns from pending collection
+router.get("/pending/:pendingId/accepted",checkAuth,studentController.get_accepted_byId) //get accepted pending thesis by Id
+router.post("/pending/:pendingId/accepted",checkAuth,studentController.confirm_pending,studentController.delete_all_pendings) // thesis is now confirmed for a specific professor
+
+router.get("/pending",checkAuth,studentController.get_pending);  //get pending thesis of userId returns from thesis collection
+router.get("/pending/:pendingId",checkAuth,studentController.get_pending_byId); //get pending thesis by id
+router.post("/pending",checkAuth,studentController.create_pending); //create pending thesis
 
 router.get("/thesis",checkAuth,studentController.get_thesis); //get thesis student
 
