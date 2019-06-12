0cebfb9d72247a0a2ba2366768abcc6b66a5967b
Michail Chatzianastasis <mixalisx97@gmail.com>
2019-06-12 18:26:24 +0300

    Add university controller, university route

---
+++ backend/controllers/university.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const University= require('../models/university');
+
+
+exports.get_all=(req,res,next) => {
+    var perPage = 2
+    var page = req.query.page || 1
+    var query= {pending:false}
+    if(req.query.university!=null)
+       query['university']=req.query.university
+    var count;
+    console.log(query)
+    query={}
+    University.countDocuments(query)
+    .then(result=> { 
+      count=result
+      University.find(query)
+        .skip((perPage * page) - perPage)
+        .limit(perPage)
+        .exec()
+        .then(docs => {
+          const response = {
+              docs:docs,
+              count:count,
+              pages: Math.ceil(count / perPage),
+          }
+          res.status(200).json(response);
+      })
+    })
+        .catch(err => {
+          console.log(err+"wjat");
+          res.status(500).json({
+            error: err
+          });
+        })
+  }
+
+  exports.get_byId= (req,res,next) => {
+    University.findById({_id:req.params.universityId})
+      .exec()
+      .then(doc => {
+        if(doc!=null)
+          res.status(200).json(doc);
+        else 
+          res.status(404).json({message : 'University not found'})
+      })
+      .catch(err => {
+        console.log(err+"wjat");
+        res.status(500).json({
+          error: err
+        });
+      })
+}
---
+++ backend/routes/university.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+const universityController = require('../controllers/university');
+const checkAuth = require('../middleware/check-auth');
+
+
+//URL: /thesis
+
+router.get("/", universityController.get_all);    //get all universities
+
+router.get("/:universityId", universityController.get_byId) //get university by id
+
+
+//router.delete("/:userId", checkAuth, thesisController.user_delete); // 
+//search thesis
+module.exports = router;
