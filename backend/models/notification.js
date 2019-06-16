b3d11a8d4fd0473025a37d026ee968bd6d78c8be
Michail Chatzianastasis <mixalisx97@gmail.com>
2019-06-16 22:02:46 +0300

    add notification model

---
+++ backend/models/notification.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const notificationSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    creator_external:{ type: mongoose.Schema.Types.ObjectId, ref:'External'},    // can be external or student
+    creator_user: { type: mongoose.Schema.Types.ObjectId, ref:'User' },     
+    receiver_external : { type: mongoose.Schema.Types.ObjectId, ref:'External'},
+    receiver_user : { type: mongoose.Schema.Types.ObjectId, ref:'User' }, 
+    text: String,
+    ccreated_time: Date
+});
+
+module.exports = mongoose.model('Pending', notificationSchema,'pending');
