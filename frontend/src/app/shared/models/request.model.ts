cac6183cddce91232182ab83d47581bfa0e6d79f
Michail Chatzianastasis <mixalisx97@gmail.com>
2019-06-29 18:34:09 +0300

    add request model in frontend

---
+++ frontend/src/app/shared/models/request.model.ts
@@ -0,0 +1,14 @@
+
+
+
+    export class Request {
+
+        public student: String;
+        public professor: String;
+        public thesis: String;
+        public text: String;
+        public created_time: Date;
+        public accepted_fromProfessor:Boolean;
+
+    }
+
