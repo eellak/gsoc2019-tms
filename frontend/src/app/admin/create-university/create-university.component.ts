eaf93a0e597309ece1c616d8495ea15e7522b200
Michail Chatzianastasis <mixalisx97@gmail.com>
2019-07-18 02:36:21 +0300

    add create-university in admin dashboard

--- frontend/src/app/admin/create-university/create-university.component.ts
+++ frontend/src/app/admin/create-university/create-university.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { first } from 'rxjs/operators';
+import { AlertService } from '../../shared/services/alert.service';
+import {AuthenticationService} from '../../shared/services/authentication.service';
+import { AdminService } from './../../shared/services/admin.service';
 
 @Component({
   selector: 'app-create-university',
@@ -6,10 +12,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-university.component.css']
 })
 export class CreateUniversityComponent implements OnInit {
+  CreateUniversityForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+  message
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: AuthenticationService,
+    private alertService: AlertService,
+    private adminService: AdminService
+     )  { }
 
   ngOnInit() {
-  }
+    this.CreateUniversityForm = this.formBuilder.group({
+        name: ['', Validators.required]
+    });
+            
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+
+   }
+
+   get f() { return this.CreateUniversityForm.controls; }
+
+   
+   onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.CreateUniversityForm.invalid) {
+        return;
+    }
+    this.loading = true;
+    this.adminService.createUniversity(this.f.name.value)
+        .subscribe(
+            (data:any) => {
+                this.message=data;
+                this.router.navigate(['../university'], { relativeTo: this.route})
+            },
+            error => {
+              console.log(error)
+                this.alertService.error(error);
+                this.loading = false;
+            });
+}
 
 }
