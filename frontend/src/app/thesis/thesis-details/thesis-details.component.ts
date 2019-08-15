a018a769f6d68b63391f53262324958132dc5888
Michail Chatzianastasis <mixalisx97@gmail.com>
2019-08-15 22:55:58 +0300

    add pdf files in thesis-details

--- frontend/src/app/thesis/thesis-details/thesis-details.component.ts
+++ frontend/src/app/thesis/thesis-details/thesis-details.component.ts
@@ -4,6 +4,10 @@ import { Subscription } from 'rxjs';
 import { AuthenticationService } from '../../shared/services/authentication.service';
 import { SharedService } from '../../shared/services/shared.service';
 import { AlertService } from '../../shared/services/alert.service';
+import { MatDividerModule, MatCardModule, MatProgressBarModule } from '@angular/material';
+import { ProfessorService } from '../../shared/services/professor.service';
+import {Sort} from '@angular/material/sort';
+
 
 @Component({
   selector: 'app-thesis-details',
@@ -21,13 +25,16 @@ export class ThesisDetailsComponent implements OnInit , OnDestroy{
   pager:any={}
   id;
   isLoaded:boolean;
+  files;
 
   constructor( 
               private router : Router,
               private authenticationService: AuthenticationService,
               private route: ActivatedRoute,
               private sharedService:SharedService,
-              private alertService:AlertService ) {}
+              private alertService:AlertService,
+              private professorService:ProfessorService
+             ) {}
 
   ngOnInit() {
     this.routeSub = this.route.params.subscribe(params => {
@@ -42,6 +49,7 @@ export class ThesisDetailsComponent implements OnInit , OnDestroy{
            this.creator_student=data.creator_student;
            console.log(this.thesis)
            this.isLoaded=true;
+           this.getFiles()
        },
        error => {
            this.alertService.error(error);
@@ -49,7 +57,73 @@ export class ThesisDetailsComponent implements OnInit , OnDestroy{
        });
     });
    }
-  
+
+   compare(a: number | string | boolean, b: number | string | boolean, isAsc: boolean) {
+    return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+  }
+
+  sortData(sort: Sort) {
+    const data = this.files.slice();
+    this.files= data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+       switch (sort.active) {
+         case 'file': return this.compare(a.title, b.title, isAsc);
+        default: return 0;
+      }
+    });
+  }
+
+   getFiles() {
+    this.loading=true;
+    this.professorService.getFilesThesis(this.id)
+    .subscribe(
+      (files:any) => {
+           console.log(files)
+           this.files=files
+          this.loading=false;
+        },
+      error => {
+          this.alertService.error(error);
+       });
+  }
+
+
+  createAndDownloadBlobFile(body, filename) {
+    const blob = new Blob([body],{type: "application/pdf"});
+    const fileName = `${filename}`;
+    if (navigator.msSaveBlob) {
+      // IE 10+
+      navigator.msSaveBlob(blob, fileName);
+    } else {
+      const link = document.createElement('a');
+      // Browsers that support HTML5 download attribute
+      if (link.download !== undefined) {
+        const url = URL.createObjectURL(blob);
+        link.setAttribute('href', url);
+        link.setAttribute('download', fileName);
+        link.style.visibility = 'hidden';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      }
+    }
+  }
+
+  downloadDraft(file) {
+      this.loading=true;
+       this.professorService.getFile(file._id)
+      .subscribe(
+        (file:any) => {
+           console.log("Returned file"+file.file_name)
+             var byteArray = new Uint8Array(file.file_data.data);
+             this.createAndDownloadBlobFile(byteArray, file.file_name);
+            this.loading=false;
+          },
+        error => {
+            this.alertService.error(error);
+         });
+    }
+      
 
   ngOnDestroy() {
     this.routeSub.unsubscribe();
