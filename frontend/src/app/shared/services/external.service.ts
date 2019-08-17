ede9e061a4ad522fa8f53bda0b79da78a34d5d97
Michail Chatzianastasis <mixalisx97@gmail.com>
2019-08-17 13:05:10 +0300

    add external service

---
+++ frontend/src/app/shared/services/external.service.ts
@@ -0,0 +1,56 @@
+import { environment } from './../../../environments/environment';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHandler, HttpErrorResponse } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import {Observable} from 'rxjs';
+ 
+
+
+@Injectable({ providedIn: 'root' })
+export class ExternalService {
+ 
+    constructor(private http: HttpClient) {}
+
+
+    getPendings(page) {
+        return this.http.get(environment.apiUrl+`/external/pending?page=${page}`)
+    }
+
+    getPendingById(pendingId) {
+        return this.http.get(environment.apiUrl+`/external/pending/${pendingId}`)
+    }
+
+    createPending(thesis) {
+        return this.http.post(environment.apiUrl+`/external/pending`,{thesis:thesis})
+    }
+
+    deletePending(thesis) {
+        return this.http.delete(environment.apiUrl+`/external/pending/${thesis._id}`)
+   }
+   
+   //get accepted pending
+
+   getAcceptedPendings() {
+           return this.http.get(environment.apiUrl+`/external/pending/accepted`);
+   }
+
+   getAcceptedPendingById(id) {
+       return this.http.get(environment.apiUrl+`/external/pending/${id}/accepted`);
+   }
+
+   postAcceptedPending(id) {
+       return this.http.post(environment.apiUrl+`/external/pending/${id}/accepted`,{});
+   }
+
+   postPdfThesis(fileToUpload: File, ThesisId): Observable<boolean> {
+    const endpoint = environment.apiUrl+`/external/thesis/pdf/${ThesisId}`;
+    const formData: FormData = new FormData();
+    formData.append('pdf', fileToUpload, fileToUpload.name);
+    return this.http
+        .post(endpoint, formData,{responseType:'arraybuffer' as 'json'})
+        .pipe(map(() => { return true; }))
+}
+ 
+
+
+}
