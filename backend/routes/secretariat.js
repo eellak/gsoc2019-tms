38a66faf65ad8ff3314047556aa56d71dbaa3780
Michail Chatzianastasis <mixalisx97@gmail.com>
2019-08-18 14:43:40 +0300

    Add get-professors routes in secretariat

--- backend/routes/secretariat.js
+++ backend/routes/secretariat.js
@@ -13,6 +13,7 @@ router.all("/*",checkAuth,secretariatController.is_secretariat)
 router.get("/get_students",secretariatController.get_students_pages); //get all students
 router.get("/get_students_not_assigned",secretariatController.get_students) //get students seperated in assigned and not assigned to thesis
 
+router.get("/get_professors",secretariatController.get_professors) // get all professors
 
 router.post("/notify_student/:studentId",secretariatController.notify_student);  //notify students that dont have assigned a thesis
 
