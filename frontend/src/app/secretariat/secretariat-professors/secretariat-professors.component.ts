b365134de3e7d869ad61a320181ef1f259bc2f10
Michail Chatzianastasis <mixalisx97@gmail.com>
2019-08-18 14:43:22 +0300

    add secretariat -professor component

--- frontend/src/app/secretariat/secretariat-professors/secretariat-professors.component.ts
+++ frontend/src/app/secretariat/secretariat-professors/secretariat-professors.component.ts
@@ -1,4 +1,7 @@
+import { AlertService } from './../../shared/services/alert.service';
+import { SecretariatService } from './../../shared/services/secretariat.service';
 import { Component, OnInit } from '@angular/core';
+import {Sort} from '@angular/material/sort';
 
 @Component({
   selector: 'app-secretariat-professors',
@@ -7,9 +10,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SecretariatProfessorsComponent implements OnInit {
 
-  constructor() { }
+  loading=false;
+  count=0;
+  pager:any={}
+  sortedData:any=[];
+  assigned;
+  not_assigned;
+
+  constructor( private secretariatService:SecretariatService,
+                private alertService:AlertService
+              ) { }
 
   ngOnInit() {
+    this.loading=true;
+     this.getProfessors(1);
+
   }
+
+  compare(a: number | string | boolean, b: number | string | boolean, isAsc: boolean) {
+    return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+}
+
+
+sortData(sort: Sort) {
+  const data = this.sortedData.slice();
+  this.sortedData = data.sort((a, b) => {
+    const isAsc = sort.direction === 'asc';
+     switch (sort.active) {
+      case 'name': return this.compare(a.name, b.name, isAsc);
+      case 'lastname': return this.compare(a.lastname, b.lastname, isAsc);
+      case 'email': return this.compare(a.email, b.email, isAsc);
+      default: return 0;
+    }
+  });
+
+}
+
+getProfessors(page) {
+  this.secretariatService.getProfessors(page)
+  .subscribe(
+  (data:any) => {
+      console.log(data);
+      this.count=data.count;
+      this.pager.count=data.count;
+      this.pager.pages= data.pages;
+      this.pager.currentPage=page;
+      this.sortedData=data.docs;
+      this.loading=false;
+   },
+  error => {
+      this.alertService.error(error);
+      this.loading = false;
+  });
+}
+ 
 
 }
