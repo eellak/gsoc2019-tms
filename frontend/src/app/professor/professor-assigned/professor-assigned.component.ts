e141c452f69fe90faec6b56fce72fbd792dbce76
Michail Chatzianastasis <mixalisx97@gmail.com>
2019-08-19 13:39:07 +0300

    change formating

--- frontend/src/app/professor/professor-assigned/professor-assigned.component.ts
+++ frontend/src/app/professor/professor-assigned/professor-assigned.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertService } from './../../shared/services/alert.service';
 import { ProfessorService } from './../../shared/services/professor.service';
-import {Sort} from '@angular/material/sort';
+import { Sort } from '@angular/material/sort';
 
 
 @Component({
@@ -10,19 +10,19 @@ import {Sort} from '@angular/material/sort';
   styleUrls: ['./professor-assigned.component.css']
 })
 export class ProfessorAssignedComponent implements OnInit {
-  theses:any=[];
+  theses: any = [];
   loading;
-  count=0;
-  pager:any={}
-  sortedData:any=[];
-  drafts={};
+  count = 0;
+  pager: any = {}
+  sortedData: any = [];
+  drafts = {};
 
-  constructor(private professorService:ProfessorService, private alertService:AlertService) { }
+  constructor(private professorService: ProfessorService, private alertService: AlertService) { }
 
   ngOnInit() {
-    this.loading=true;
-   this.getAssignedThesis(1)
-   }
+    this.loading = true;
+    this.getAssignedThesis(1)
+  }
 
 
   compare(a: number | string | boolean, b: number | string | boolean, isAsc: boolean) {
@@ -33,7 +33,7 @@ export class ProfessorAssignedComponent implements OnInit {
     const data = this.sortedData.slice();
     this.sortedData = data.sort((a, b) => {
       const isAsc = sort.direction === 'asc';
-       switch (sort.active) {
+      switch (sort.active) {
         case 'created_time': return this.compare(a.created_time, b.created_time, isAsc);
         case 'title': return this.compare(a.title, b.title, isAsc);
         case 'student': return this.compare(a.lastname, b.lastname, isAsc);
@@ -47,48 +47,48 @@ export class ProfessorAssignedComponent implements OnInit {
 
   getAssignedThesis(page) {
     this.professorService.getAssigned(page)
-    .subscribe(
-    (data:any) => {
-        console.log(data)
+      .subscribe(
+        (data: any) => {
+          console.log(data)
           //this.alertService.success('Get user information successful', true);
-        this.sortedData=data.docs;
-        this.count=data.count;
-        this.pager.count=data.count;
-        this.pager.pages= data.pages;
-        this.pager.currentPage=page;
-        this.loading=false;
-        for(let i=0;i<this.count;i++) {
-          this.getDrafts(this.sortedData[i]._id,i)
-        }
-        
-       
-     },
-    error => {
-        this.alertService.error(error);
-        this.loading = false;
-    });
+          this.sortedData = data.docs;
+          this.count = data.count;
+          this.pager.count = data.count;
+          this.pager.pages = data.pages;
+          this.pager.currentPage = page;
+          this.loading = false;
+          for (let i = 0; i < this.count; i++) {
+            this.getDrafts(this.sortedData[i]._id, i)
+          }
+
+
+        },
+        error => {
+          this.alertService.error(error);
+          this.loading = false;
+        });
   }
 
-  getDrafts(assigned_id,index) {
-    this.loading=true;
-    console.log(assigned_id+" index " + index)
-     this.professorService.getDrafts(assigned_id)
-    .subscribe(
-      (drafts:any) => {
-           console.log(drafts)
-           this.drafts[index]=drafts
-            this.loading=false;
+  getDrafts(assigned_id, index) {
+    this.loading = true;
+    console.log(assigned_id + " index " + index)
+    this.professorService.getDrafts(assigned_id)
+      .subscribe(
+        (drafts: any) => {
+          console.log(drafts)
+          this.drafts[index] = drafts
+          this.loading = false;
         },
-      error => {
+        error => {
           console.log(error)
           this.alertService.error(error);
-       });
+        });
   }
-  
-  
+
+
 
   createAndDownloadBlobFile(body, filename) {
-    const blob = new Blob([body],{type: "application/pdf"});
+    const blob = new Blob([body], { type: "application/pdf" });
     const fileName = `${filename}`;
     if (navigator.msSaveBlob) {
       // IE 10+
@@ -109,18 +109,18 @@ export class ProfessorAssignedComponent implements OnInit {
   }
 
   downloadDraft(draft) {
-       this.professorService.getDraftById(draft.assigned_thesis,draft._id)
+    this.professorService.getDraftById(draft.assigned_thesis, draft._id)
       .subscribe(
-        (draft:any) => {
-           console.log(draft)
-             var byteArray = new Uint8Array(draft[0].data.data);
-             this.createAndDownloadBlobFile(byteArray, draft[0].name);
-            this.loading=false;
-          },
+        (draft: any) => {
+          console.log(draft)
+          var byteArray = new Uint8Array(draft[0].data.data);
+          this.createAndDownloadBlobFile(byteArray, draft[0].name);
+          this.loading = false;
+        },
         error => {
-            this.alertService.error(error);
-         });
-    }
-      
+          this.alertService.error(error);
+        });
+  }
+
 
 }
