1a70f46ba7c0b542290d9ff1c505b51068f69a32
Michail Chatzianastasis <mixalisx97@gmail.com>
2019-08-20 20:16:20 +0300

    change supervision_request, remove acceptedFromProfessor

--- backend/models/supervision_requests.js
+++ backend/models/supervision_requests.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 
-//students requests for thesis selection 
+//requests for supervision thesis
+
 const supervision_requestSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     student: { type: mongoose.Schema.Types.ObjectId, ref:'User'},
     professor: { type: mongoose.Schema.Types.ObjectId, ref:'User'},
     dst_professor: { type: mongoose.Schema.Types.ObjectId, ref:'User'},
-    thesis: {type: mongoose.Schema.Types.ObjectId, ref:'Thesis'},
+    assigned_thesis: {type: mongoose.Schema.Types.ObjectId, ref:'Assigned_Thesis'},
     text: String,
     created_time: Date,
-    accepted_fromProfessor:Boolean
     });
 
 module.exports = mongoose.model('Supervision_Request', supervision_requestSchema);
